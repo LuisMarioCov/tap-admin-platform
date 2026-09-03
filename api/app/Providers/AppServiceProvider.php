b1e71145d8e7d3a79e67377e4942b0b76a9494da
DelgadoCovarrubiasLuisMario <ldelgado17@ucol.com>
2026-09-03 11:49:14 -0600

    Complete TAP exam

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\PersonalAccessToken;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Sanctum\Sanctum;
@@ -16,8 +17,12 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
+        if ($this->app->environment('production')) {
+            $url->forceScheme('https');
+        }
+
         Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
 
         RateLimiter::for('login', function (Request $request) {
